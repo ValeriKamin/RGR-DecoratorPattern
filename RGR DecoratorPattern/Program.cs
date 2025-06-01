56f0ebf85d74003bb5a5e081f0f203dde2ebf459
ValeriKamin <v.a.murzin@student.khai.edu>
2025-06-01 14:01:39 +0300

    Добавьте файлы проекта.

---
+++ RGR DecoratorPattern/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public interface INotifier
+{
+    void Send();
+}
+
+public class EmailNotifier : INotifier
+{
+    public void Send()
+    {
+        Console.WriteLine("Надiслано email-повiдомлення.");
+    }
+}
+
+public abstract class NotifierDecorator : INotifier
+{
+    protected INotifier wrappee;
+
+    public NotifierDecorator(INotifier notifier)
+    {
+        wrappee = notifier;
+    }
+
+    public virtual void Send()
+    {
+        wrappee.Send();
+    }
+}
+
+public class SMSNotifier : NotifierDecorator
+{
+    public SMSNotifier(INotifier notifier) : base(notifier) { }
+
+    public override void Send()
+    {
+        base.Send();
+        Console.WriteLine("Надiслано SMS.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        INotifier notifier = new SMSNotifier(new EmailNotifier());
+        notifier.Send();
+    }
+}
